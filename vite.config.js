7081698db3fddafa90cd29ea1ecac3b254143aa4
Iris <iris0952@gmail.com>
2024-08-17 17:44:53 +0800

    git init

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Server',
+  base: '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
